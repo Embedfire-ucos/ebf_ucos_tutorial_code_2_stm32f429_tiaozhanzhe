6d0830a32183e49557fa1f0caaa90f8e58dd80e2
flyleaf <flyleaf91@163.com>
2020-01-13 20:58:17 +0800

    迁移仓库

---
+++ 20-消息队列/1.发送和接收消息/Project/RVMDK(uv5)/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'WF-STM32F429' 
+ * Target:  '消息队列' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ 21-信号量/1-二值信号量-标志事件/Project/RVMDK(uv5)/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'WF-STM32F429' 
+ * Target:  '多值信号量' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
